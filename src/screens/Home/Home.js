8c36d30f511e6fd9fae91522298469f72b951c03
rafa-Nobre <denys.nobre@outlook.com>
2022-09-28 13:19:27 -0300

    Importando componentes

---
+++ src/screens/Home/Home.js
@@ -0,0 +1,26 @@
+import React, {useEffect} from 'react';
+import MovieListing from '../../components/MovieListing/MovieListing';
+import './Home.scss';
+import {useDispatch} from 'react-redux';
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from '../../features/movies/movieSlice';
+
+const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
+  );
+};
+
+export default Home;
