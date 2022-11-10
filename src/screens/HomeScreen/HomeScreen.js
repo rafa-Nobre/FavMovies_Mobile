33851c95249d196695ce9adcd05711322434471e
rafa-Nobre <denys.nobre@outlook.com>
2022-11-10 16:16:43 -0300

    Ajustes Homescreen

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -3,14 +3,6 @@ import {View, Text} from 'react-native';
 //import MaterialBottomNavigator from '../../components/BottomTab/MaterialBottomNavigator';
 
 const HomeScreen = () => {
-  // const getUsername = async user => {
-  //   try {
-  //     const response = await Auth.userAttributes(user.username);
-  //     console.log('HomeScreen ~ getUsername ~ 9 ~ response: ', response);
-  //   } catch (e) {
-  //     console.log('Oops', e.message);
-  //   }
-  // };
   return (
     <View>
       <Text style={{fontSize: 24, alignSelf: 'center'}}>Bem vindo</Text>
