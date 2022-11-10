85b5c596a9d1c27efe0b07906ac88a3ff1d629b9
rafa-Nobre <denys.nobre@outlook.com>
2022-11-10 16:22:29 -0300

    Ajustes Home

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -3,6 +3,9 @@ import {View, Text} from 'react-native';
 //import MaterialBottomNavigator from '../../components/BottomTab/MaterialBottomNavigator';
 
 const HomeScreen = () => {
+  const signOut = () => {
+    Auth.signOut();
+  };
   return (
     <View>
       <Text style={{fontSize: 24, alignSelf: 'center'}}>Bem vindo</Text>
--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import {View, Image, useWindowDimensions, Alert} from 'react-native';
-import Logo from '../../../assets/images/user.png';
+import Logo from '../../../assets/images/logo.png';
 import CustomInput from '../../components/CustomInput';
 import CustomButton from '../../components/CustomButton';
 import styles from './styles';
